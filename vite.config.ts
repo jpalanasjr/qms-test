bec9888a2c1690aca19a4ea24c2b46dafc2ab1da
Julius Palanas Jr <jpalanasjr@gmail.com>
2025-11-16 20:52:20 +0800

    Add GitHub Pages SPA redirect

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
- 
+ base: mode === "production" ? "/qms-test/" : "/",
   server: {
     host: "::",
     port: 8080,
@@ -16,5 +16,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/qms-test",
+  
 }));
